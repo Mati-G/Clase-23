3f3cbbfa1d8b6355bcf32a67d50db948708118ad
Mati-G <Matiasn.gimenez@hotmail.com>
2021-05-28 15:23:41 -0300

    Levantando un servidor

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express'); //Requiero express
 const app = express(); //Requiero la funcionalidad de Express
+const port = 3000; // Numero del servidor
 
-app.listen(3000, () => console.log('Algo'));
+app.listen(port, () => console.log('Algo' + port));
 
 
